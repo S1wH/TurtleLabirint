e3c7ad7217cddec4d792fbd398e8965d3cd31216
Павел Гомозов <gomozov.pavel@yandex.ru>
2020-11-24 21:35:05 +0300

    exit count step and exit show step unicode symbols

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ class LabirintTurtle:
     def __init__(self):
         self.map = []
         self.exit = None
+        self.turtle = None
+        self.path = []
 
     def load_map(self, file_name):
         file = open(file_name, 'r')
@@ -14,8 +16,9 @@ class LabirintTurtle:
         try:
             pos_x = int(line)
             pos_y = int(file.readline())
-            self.map[pos_x][pos_y] = 'A'
-            result = self.check_map(pos_x, pos_y)
+            self.map[pos_x][pos_y] = chr(128034)
+            self.turtle = [pos_x, pos_y]
+            result = self.check_map()
             if result is None:
                 print('Введите название файла')
                 file_name = input()
@@ -30,27 +33,30 @@ class LabirintTurtle:
         if turtle is True:
             for i in range(len(self.map)):
                 for j in range(len(self.map[i])):
-                    print(self.map[i][j], end=' ')
+                    print(self.map[i][j], end='\t')
                 print()
 
         # if turtle is false
         else:
             for i in range(len(self.map)):
                 for j in range(len(self.map[i])):
-                    if self.map[i][j] != 'A':
-                        print(self.map[i][j], end=' ')
+                    if self.map[i][j] != chr(128034):
+                        print(self.map[i][j], end='\t')
                     else:
-                        print(' ', end=' ')
+                        print(' ', end='\t')
                 print()
 
-    def check_map(self, pos_x, pos_y):
+    def check_map(self):
+        pos_x = self.turtle[0]
+        pos_y = self.turtle[1]
+
         # check if there is an exit and if there any other symbols except for * and ' '
         for i in range(len(self.map)):
             for j in range(len(self.map[i])):
                 if i == 0 or i == -1 or j == 0 or j == -1:
                     if self.map[i][j] == ' ':
                         self.exit = [i, j]
-                if self.map[i][j] != ' ' and self.map[i][j] != '*' and self.map[i][j] != 'A':
+                if self.map[i][j] != ' ' and self.map[i][j] != '*' and self.map[i][j] != chr(128034):
                     return None
 
         # check if turtle is in a wall
@@ -62,21 +68,53 @@ class LabirintTurtle:
             return None
         # check if turtle can get to the exit
         while pos_x != self.exit[0] or pos_y != self.exit[1]:
+            self.path.append([pos_x, pos_y])
             if pos_x > self.exit[0] and self.map[pos_x - 1][pos_y] == ' ':
                 pos_x -= 1
-            if pos_x < self.exit[0] and self.map[pos_x + 1][pos_y] == ' ':
+            elif pos_x < self.exit[0] and self.map[pos_x + 1][pos_y] == ' ':
                 pos_x += 1
-            if pos_y > self.exit[1] and self.map[pos_x][pos_y - 1] == ' ':
+            elif pos_y > self.exit[1] and self.map[pos_x][pos_y - 1] == ' ':
                 pos_y -= 1
-            if pos_y < self.exit[1] and self.map[pos_x][pos_y + 1] == ' ':
+            elif pos_y < self.exit[1] and self.map[pos_x][pos_y + 1] == ' ':
                 pos_y += 1
             else:
                 break
+        # check if turtle is in the exit
         if pos_x != self.exit[0] or pos_y != self.exit[1]:
+            self.path = []
             return None
+        self.path.append(self.exit)
         return 'OK'
+
+    def exit_count_step(self):
+        count = 1
+        pos_x = self.turtle[0]
+        pos_y = self.turtle[1]
+        # move position of turtle until it is in the exit
+        while pos_x != self.exit[0] or pos_y != self.exit[1]:
+            if pos_x > self.exit[0] and self.map[pos_x - 1][pos_y] == ' ':
+                pos_x -= 1
+                count += 1
+            if pos_x < self.exit[0] and self.map[pos_x + 1][pos_y] == ' ':
+                pos_x += 1
+                count += 1
+            if pos_y > self.exit[1] and self.map[pos_x][pos_y - 1] == ' ':
+                pos_y -= 1
+                count += 1
+            if pos_y < self.exit[1] and self.map[pos_x][pos_y + 1] == ' ':
+                pos_y += 1
+                count += 1
+        return count
+
+    def exit_show_step(self):
+        # make steps
+        for positions in self.path:
+            self.map[positions[0]][positions[1]] = chr(128062)
+        self.show_map()
 
 
 lab = LabirintTurtle()
-lab.load_map('1.txt')
+lab.load_map('2.txt')
 lab.show_map(turtle=True)
+print(lab.exit_count_step())
+lab.exit_show_step()
